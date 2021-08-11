f1a790abd0dbd31102b9b5e01a00f60e0cf05b44
Creg Sullivan <cregcsullivan1024@gmail.com>
2021-08-10 19:26:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Demo
+{
+    public class MyApplication
+    {
+        public static void Main(string[] args)
+        {
+            int[] list = { 98, 23, 97, 36, 77 };
+            Console.WriteLine("Original Unsorted List");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            //sort
+            Array.Sort(list);
+            // Descending order
+            Array.Reverse(list);
+            Console.WriteLine("\nSorted List");
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+        }
+    }
+}
